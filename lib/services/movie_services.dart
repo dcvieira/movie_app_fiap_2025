cc12679fed0bd5eb08216eb0f3afc312e45eecc9
DIEGO CAMILO MARTINS VIEIRA <dcvieira@tjsp.jus.br>
2025-08-25 00:00:59 -0300

    aula01 before

--- lib/services/movie_services.dart
+++ lib/services/movie_services.dart
@@ -1,21 +1,6 @@
-import 'dart:convert';
-
-import 'package:http/http.dart' as http;
 import 'package:movie_app_fiap25/common/constants.dart';
-import 'package:movie_app_fiap25/models/movie_model.dart';
 
 class MovieServices {
   final baseUrl = 'https://api.themoviedb.org/3/';
   final key = '?api_key=$apiKey';
-
-  Future<Result> getTopRatedMovies() async {
-    var endPoint = 'movie/top_rated';
-    final url = '$baseUrl$endPoint$key';
-
-    final response = await http.get(Uri.parse(url));
-    if (response.statusCode == 200) {
-      return Result.fromJson(jsonDecode(response.body));
-    }
-    throw Exception('Failed to load top rated movies');
-  }
 }
--- lib/widgets/botton_nav.dart
+++ lib/widgets/botton_nav.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:movie_app_fiap25/pages/home/home_page.dart';
-import 'package:movie_app_fiap25/pages/top_rated/top_rated_page.dart';
 
 class BottomNav extends StatefulWidget {
   const BottomNav({super.key});
@@ -10,39 +8,8 @@ class BottomNav extends StatefulWidget {
 }
 
 class _BottomNavState extends State<BottomNav> {
-  int paginaAtual = 0;
-  PageController pc = PageController(initialPage: 0);
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: PageView(
-        controller: pc,
-        onPageChanged: (page) {
-          setState(() {
-            paginaAtual = page;
-          });
-        },
-        children: const [HomePage(), TopRatedPage()],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: paginaAtual,
-        iconSize: 30,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.trending_up),
-            label: 'Top Rated',
-          ),
-        ],
-        onTap: (pagina) {
-          pc.animateToPage(
-            pagina,
-            duration: const Duration(milliseconds: 400),
-            curve: Curves.ease,
-          );
-        },
-      ),
-    );
+    return Center(child: Text('Bottom Navigation'));
   }
 }
