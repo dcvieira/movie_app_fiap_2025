8819004b845c6bb1448c6fd142c2215c075bef37
DIEGO CAMILO MARTINS VIEIRA <dcvieira@tjsp.jus.br>
2025-09-01 22:58:27 -0300

    aula02 after

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,10 +1,117 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app_fiap25/models/movie_model.dart';
+import 'package:movie_app_fiap25/pages/home/widgets/movies_horizontal_list.dart';
+import 'package:movie_app_fiap25/pages/home/widgets/nowplayng_list.dart';
+import 'package:movie_app_fiap25/services/movie_services.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final MovieServices movieServices = MovieServices();
+  late Future<Result> popular;
+  late Future<Result> nowPlaying;
+  late Future<Result> upcoming;
+
+  @override
+  void initState() {
+    popular = movieServices.getPopularMovies();
+    nowPlaying = movieServices.getNowPlayingMovies();
+    upcoming = movieServices.getUpcomingMovies();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Home Page'));
+    return Scaffold(
+      appBar: AppBar(title: const Text('Movie App')),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                child: Text(
+                  'Now Playing',
+                  style: TextStyle(
+                    color: Colors.white54,
+                    fontWeight: FontWeight.w300,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              FutureBuilder(
+                future: nowPlaying,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                  if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  }
+                  if (snapshot.hasData) {
+                    return NowPlayingList(movies: snapshot.data!.movies);
+                  }
+                  return const Center(child: Text('No data found'));
+                },
+              ),
+              const SizedBox(height: 20),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                child: Text(
+                  'Popular',
+                  style: TextStyle(
+                    color: Colors.white54,
+                    fontWeight: FontWeight.w300,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              FutureBuilder(
+                future: popular,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                  if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  }
+                  return MoviesHorizontalList(movies: snapshot.data!.movies);
+                },
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                child: Text(
+                  'Upcoming',
+                  style: TextStyle(
+                    color: Colors.white54,
+                    fontWeight: FontWeight.w300,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              FutureBuilder(
+                future: upcoming,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                  if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  }
+                  return MoviesHorizontalList(movies: snapshot.data!.movies);
+                },
+              ),
+              const SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
